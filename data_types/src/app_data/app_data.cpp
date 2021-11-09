52dee04528d8ebfc2332392c0dfd8656494a5601
Treborsky <rob.koziarski2k@gmail.com>
2021-11-09 20:22:54 +0100

    working on parsing csv file

--- data_types/src/app_data/app_data.cpp
+++ data_types/src/app_data/app_data.cpp
@@ -10,13 +10,25 @@ void AppData::parseLine(std::string& line) {
      * i - ingredients
      * s - supplies
      * and then a list of the items of the type:
-     * r - list of ingredients for the recipe
+     * r - list of ingredients for the recipe, then prep time
      * i - list of ingredients that we currently store
      * s - list of available items in the store
      * */
     std::vector<std::string> tokenList = app::strSplit(line, ',');
     if (tokenList[0] == "r") { // parse the list of ingredients and put together a recipe
-        std::size_t i = 0;
+        if (tokenList.size() - 1 % 3 != 0) {
+            return;
+        }
+        std::vector<Ingredient> ingredients();
+
+        std::size_t i = 1;
+        bool isWord;
+        for(; i < tokenList.size() - 1; i += 1) {
+
+            if (isWord) {
+
+            }
+        }
     } else if (tokenList[0] == "i") { // store the list of ingredients
 
     } else if (tokenList[0] == "s") { // update the available products
